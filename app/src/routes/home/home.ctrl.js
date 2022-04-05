464e71ad5ca53d429b3be6027eaf112774b5b25d
kwy <kwy@Juness-MacBook-Pro.local>
2022-04-05 21:18:32 +0900

    model user 추가 및 정리

--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const UserStorage = require("../../models/UserStorage");
+const User = require("../../models/User");
 
 const output = {
     home : (req, res) => {
@@ -19,23 +19,9 @@ const output = {
 
 const process = {
     login : (req, res) => {
-        const id = req.body.id,
-        password = req.body.password;
-        
-        const users = UserStorage.getUsers("id", "password");
-
-        const response = {};
-        if(users.id.includes(id)){
-            const idx = users.id.indexOf(id);
-            if(users.password[idx] === password){
-                response.success = true;
-                return res.json(response);
-            }
-        }
-        
-        response.success = false;
-        response.msg = "로그인에 실패!";
-        return res.json(response);
+       const user = new User(req.body);
+       const response = user.login();
+       return res.json(response);
     },
 };
 
