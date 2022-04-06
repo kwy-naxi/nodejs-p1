fd744fb639dffb02b8dd312cebfcd55af9b329bb
kwy <kwy@Juness-MacBook-Pro.local>
2022-04-06 15:12:32 +0900

    회원가입 라우팅 구현

--- app/src/public/js/home/register.js
+++ app/src/public/js/home/register.js
@@ -11,31 +11,33 @@ registerbtn = document.querySelector("#button");
 registerbtn.addEventListener("click",register);
 
 function register(){
+    
+    if(!id.value) return alert("아이디를 입력해주세요.");
+    if (password.value !== confirmPassword.value) return alert("비밀번호가 일치하지 않습니다.");
+
     const req = {
         id : id.value,
         name : name.value,
         password : password.value,
-        confirmPassword : confirmPassword.value,
     };
-    console.log(req);
 
-    // fetch("/register", {
-    //     method: "POST",
-    //     headers:{
-    //         "Content-Type": "application/json",
-    //     },
-    //     body: JSON.stringify(req),
-    // })
-    //     .then((res) => res.json())
-    //     .then((res) => {
-    //         if (res.success) {
-    //             location.href = "/";
-    //         } else {
-    //             alert(res.msg);
-    //         }
-    //     })
-    //     .catch((err) => {
-    //         console.error("로그인 중 에러 발생!");
-    //     });
+    fetch("/register", {
+        method: "POST",
+        headers:{
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(req),
+    })
+        .then((res) => res.json())
+        .then((res) => {
+            if (res.success) {
+                location.href = "/login";
+            } else {
+                alert(res.msg);
+            }
+        })
+        .catch((err) => {
+            console.error("로그인 중 에러 발생!");
+        });
 }
 
